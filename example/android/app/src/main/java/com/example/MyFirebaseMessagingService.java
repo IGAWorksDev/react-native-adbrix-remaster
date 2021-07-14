97be12435424311c5862d85cdb43caa86b683881
yen-igaw <yen@igaworks.com>
2021-07-15 00:42:18 +0900

    FCM integration

--- example/android/app/src/main/java/com/example/MyFirebaseMessagingService.java
+++ example/android/app/src/main/java/com/example/MyFirebaseMessagingService.java
@@ -1,2 +1,21 @@
-package com.example;public class MyFirebaseMessagingService {
+package com.example;
+
+import android.util.Log;
+import com.igaworks.v2.core.AdBrixRm;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("adbrix", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+        // Send token data to Adbrix
+        AdBrixRm.setRegistrationId(token);
+    }
+
 }
