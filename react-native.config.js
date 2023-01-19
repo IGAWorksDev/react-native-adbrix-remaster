bfc4f0b3b9c9ef652ace29020688f4b9e7792205
Jonathan-igaw <jonathan.hwang@igaworks.com>
2023-01-19 11:12:57 +0900

    IOS  설정 변경 및 SDK 버전 및 설정 변경
    - IOS의 푸시를 위한 프레임워크를 구버전 AdBrixRM_XC를 사용하고 있어 AdBrixRmKit를 사용하게 변경
    - sdk 버전명 변경
    - react-native.config의 Android 링크를 null값으로 처리하여 자동으로 패키지 리스트를 생성하지 않게 변경

--- react-native.config.js
+++ react-native.config.js
@@ -4,10 +4,8 @@ const path = require('path');
 module.exports = {
   dependency: {
     platforms: {
-      ios: { podspecPath: path.join(__dirname, 'react-native-adbrix-remaster-qa.podspec') },
-      android: {
-        
-      },
+      ios: { podspecPath: path.join(__dirname, 'react-native-adbrix-remaster.podspec') },
+      android: null
     },
   },
 };
